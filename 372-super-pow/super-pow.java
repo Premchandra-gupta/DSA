769f93027be21f3a8f6215dc27380e08ec27bdea
Prem Chandra Gupta <149512766+Premchandra-gupta@users.noreply.github.com>
2026-02-15 15:17:21 +0530

    Time: 4 ms (74.77%) | Memory: 46.4 MB (65.26%) - LeetSync

---
+++ 372-super-pow/super-pow.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    static final int MOD = 1337;
+    
+    public int superPow(int a, int[] b) {
+        
+        int result = 1;
+        a %= MOD;
+        
+        for (int digit : b) {
+            result = pow(result, 10) * pow(a, digit) % MOD;
+        }
+        
+        return result;
+    }
+    
+    private int pow(int x, int n) {
+        int result = 1;
+        
+        x %= MOD;
+        
+        while (n > 0) {
+            if (n % 2 == 1) {
+                result = result * x % MOD;
+            }
+            x = x * x % MOD;
+            n /= 2;
+        }
+        
+        return result;
+    }
+}
