3aede78ad850a57a9458c9b254289b0e986e52c2
Prem Chandra Gupta <149512766+Premchandra-gupta@users.noreply.github.com>
2026-02-15 15:08:49 +0530

    Time: 1 ms (76.54%) | Memory: 42.3 MB (77.77%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    public boolean isHappy(int n) {
+        
+        int slow = n;
+        int fast = n;
+        
+        do {
+            slow = squareSum(slow);
+            fast = squareSum(squareSum(fast));
+        } while (slow != fast);
+        
+        return slow == 1;
+    }
+    
+    private int squareSum(int num) {
+        int sum = 0;
+        
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+        
+        return sum;
+    }
+}
